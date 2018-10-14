282859472d0829a72aeca32b32469d6b2a219abb
derek.palmer <derek.palmer@me.com>
2018-10-14 11:08:39 +0100

    Updates

---
+++ precache-manifest.69f66fc74007c15072ace34c77a1269b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "db3496a67c5c9e2a5b02",
+    "url": "/contactmanager/static/js/main.db3496a6.chunk.js"
+  },
+  {
+    "revision": "57234ea9756493bc2d84",
+    "url": "/contactmanager/static/js/1.57234ea9.chunk.js"
+  },
+  {
+    "revision": "db3496a67c5c9e2a5b02",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "57234ea9756493bc2d84",
+    "url": "/contactmanager/static/css/1.e1002ccf.chunk.css"
+  },
+  {
+    "revision": "c93a3302c6c29e19ba84e97cb3930f6e",
+    "url": "/contactmanager/index.html"
+  }
+];
